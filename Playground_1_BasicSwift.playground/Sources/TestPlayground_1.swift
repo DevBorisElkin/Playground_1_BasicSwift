e08bb99b922c055587f06600b247d8ece97d73a9
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-03 22:16:58 +0300

    more little changes

--- Playground_1_BasicSwift.playground/Sources/TestPlayground_1.swift
+++ Playground_1_BasicSwift.playground/Sources/TestPlayground_1.swift
@@ -65,5 +65,39 @@ public class TestLesson_1
         /*
          Long comment
          */
+        
+        
+        /*
+         AnyClass - может содержать любые классы (является parent object'ом для всех классов),
+         не может содержать структуры, enum'ы, и closures
+         
+         Any - может содержать классы, структуры, enum'ы и closures (является parent object'ом для их всех)
+         */
+        
+        var someCoolArrayOfClasses: [AnyObject] = [A_1(), A_2(), A_3()]
+        
+        var coolArrayOfEnyObjects: [Any] = [A_1(), A_2(), A_3(), A_4(), A5.A5_2, "Hello", 232, { () -> () in }]
+        
+        for someVar in coolArrayOfEnyObjects{
+            switch someVar{
+            case let someIntVar as Int:
+                print("This Is int")
+                print("And I can print that variable: \(someVar)")
+            case let someStringVar as String:
+                print("this is string")
+            default:
+                print("I don't know what that is")
+        }
     }
 }
+
+class A_1{}
+class A_2{}
+class A_3{}
+struct A_4{}
+enum A5{
+    case A5_1
+    case A5_2
+    case A5_3
+}
+
